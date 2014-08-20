e47b3ab3f37ea8efb5e7f91872c9f1b7cbbef872
Piotr Jamroz <pm.jamroz@gmail.com>
2014-08-20 23:50:45 +0100

    clean save plugin

--- js/plugins/save.js
+++ js/plugins/save.js
@@ -5,43 +5,31 @@ define(['core', 'logger', 'jquery', 'saveAs', 'plugins/preview'], function (core
 	plugin.data = {
 		filename: "screenplay"
 	};
-	
-	plugin.init = function () {
-		$(document).ready(function () {
-			$('a[action="save-fountain"]').click(function () {
-				var blob = new Blob([core.script()], {
-					type: "text/plain;charset=utf-8"
-				});
-				saveAs(blob, plugin.data.filename + '.fountain');
-			});
-			$('a[action="save-dropbox-fountain"]').click(function () {
-				var encoded = window.btoa(core.script());
-				var uri = 'data:text/plain;base64,' + encoded;
-				Dropbox.save(uri, plugin.data.filename + '.fountain');
-			});
-			
-			$('a[action="save-pdf"]').click(function () {
-				preview.get_pdf().output('save', plugin.data.filename + '.pdf');
-			});	
-			$('a[action="save-dropbox-pdf"]').click(function () {
-				var uri = preview.get_pdf().output("datauristring");
-				Dropbox.save(uri, plugin.data.filename + '.pdf');
-			});	
-			$('#save-filename').keyup(function(){
-				plugin.data.filename = $(this).val();
-			});
+
+	plugin.save_as_fountain = function () {
+		var blob = new Blob([core.script()], {
+			type: "text/plain;charset=utf-8"
 		});
+		saveAs(blob, plugin.data.filename + '.fountain');
 	};
 
-	plugin.activate = function () {
-		if (!(Dropbox.isBrowserSupported()) || window.location.protocol === 'file:') {
-			$('a[action="save-dropbox-pdf"], a[action="save-dropbox-fountain"]').parent().hide();
-		}
-		$('#save-filename').val(plugin.data.filename);
+	plugin.save_as_pdf = function () {
+		preview.get_pdf().output('save', plugin.data.filename + '.pdf');
 	};
 
-	plugin.deactivate = function () {
-		log.info('save:deactivate');
+	plugin.dropbox_fountain = function () {
+		var encoded = window.btoa(core.script());
+		var uri = 'data:text/plain;base64,' + encoded;
+		Dropbox.save(uri, plugin.data.filename + '.fountain');
+	};
+
+	plugin.dropbox_pdf = function () {
+		var uri = preview.get_pdf().output("datauristring");
+		Dropbox.save(uri, plugin.data.filename + '.pdf');
+	};
+
+	plugin.is_dropbox_available = function () {
+		return Dropbox.isBrowserSupported() && window.location.protocol !== 'file:';
 	};
 
 	return plugin;
