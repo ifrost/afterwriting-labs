52d8d1dc0f1826369192c29a2a7cf5c9439cd596
Piotr Jamroz <pm.jamroz@gmail.com>
2014-08-23 01:17:40 +0100

    stats: who

--- js/dev.js
+++ js/dev.js
@@ -1,15 +1,18 @@
-define(['core', 'plugins/open', 'utils/layout'], function (core, open, layout, dev_plugin) {
+define(['core', 'plugins/open', 'utils/layout', 'plugins/stats'], function (core, open, layout, dev_plugin) {
 	if (window.__devmode) {
 		core.loaded = function () {
 
 			if (dev_plugin) {
 				open.open_sample('big_fish');
 
-				core.switch_to(dev_plugin);
+				setTimeout(function() {
+					layout.switch_to_plugin(dev_plugin.name);
+					core.switch_to(dev_plugin);
 
-				layout.show_options();
-				layout.open_content();
-				layout.switch_to_plugin(dev_plugin.name);
+					layout.show_options();
+					layout.open_content();
+				}, 200);
+				
 			}
 
 			layout.dev();
--- js/plugins/stats.js
+++ js/plugins/stats.js
@@ -6,10 +6,10 @@ define(['core', 'logger', 'd3', 'jquery', 'plugins/editor', 'utils/data', 'utils
 		log.info('stats:init');
 	};
 
-	plugin.goto = function(line) {
+	plugin.goto = function (line) {
 		editor.goto(line);
 	}
-	
+
 	plugin.activate = function () {
 		var scenes = [];
 		var days_and_nights = {
@@ -20,9 +20,27 @@ define(['core', 'logger', 'd3', 'jquery', 'plugins/editor', 'utils/data', 'utils
 			other: 0,
 			sum: 0
 		};
+		var scenes_characters = [];
+		var current_scene_characters = d3.set();
+		var all_characters_nof_scenes = {};
+		var all_characters = d3.set();
+
+		var append_characters = function () {
+			scenes_characters.push(current_scene_characters);
+			current_scene_characters.values().forEach(function (character) {
+				if (!all_characters_nof_scenes[character]) {
+					all_characters_nof_scenes[character] = 0;
+				}
+				all_characters_nof_scenes[character]++;
+			});
+			current_scene_characters = d3.set();
+
+		}
+
 		data.parsed.tokens.forEach(function (token) {
 			var type;
 			if (token.type === 'scene_heading') {
+				append_characters();
 				var suffix = token.text.substring(token.text.lastIndexOf(' - '))
 				if (suffix.indexOf('DAY') !== -1) {
 					days_and_nights.day++;
@@ -52,8 +70,82 @@ define(['core', 'logger', 'd3', 'jquery', 'plugins/editor', 'utils/data', 'utils
 				if (scenes.length) {
 					scenes[scenes.length - 1].length += token.lines.length;
 				}
+				if (token.type === "character") {
+					var character = token.text;
+					var p = character.indexOf('(');
+					if (p != -1) {
+						character = character.substring(0, p);
+					}
+					character = character.trim();
+					current_scene_characters.add(character);
+					all_characters.add(character);
+				}
+			}
+
+		});
+
+		append_characters();
+
+		var top10 = (function (data) {
+			var flat = [];
+			for (var key in data) {
+				flat.push({
+					name: key,
+					nof_scenes: data[key]
+				});
+			}
+			var compare = function (a, b) {
+				return b.nof_scenes - a.nof_scenes;
+			};
+			return flat.sort(compare).slice(0, 10);
+		})(all_characters_nof_scenes);
+
+		var top10_names = top10.map(function (character) {
+			return character.name
+		});
+		var links = [];
+		var id = function (a, b) {
+			return Math.min(a, b) + '_' + Math.max(a, b);
+		};
+		var perm = function (t) {
+			return t.map(function (item, index) {
+				return t.slice(index + 1).map(function (i) {
+					return [t[index], i]
+				})
+			}).reduce(function (prev, cur) {
+				return prev.concat(cur)
+			}, []);
+		};
+
+		var link_map = {};
+		scenes_characters.forEach(function (scene) {
+			var perms = perm(scene.values());
+			for (var i=0; i<perms.length;i++) {
+				var a = top10_names.indexOf(perms[i][0]);
+				var b = top10_names.indexOf(perms[i][1]);
+				if (a == -1 || b == -1) {continue;}
+				
+				var key = id(a,b);
+				if (!link_map[key]) {
+					link_map[key] = 0;
+				}
+				link_map[key]++;
 			}
 		});
+		
+		for (var key in link_map) {
+			var indexes = key.split('_').map(Number);
+			links.push({
+				from: indexes[0],
+				to: indexes[1],
+				scenes: link_map[key]
+			});	
+		}
+
+		plugin.who_with_who = {
+			characters: top10,
+			links: links
+		};
 
 		plugin.data.days_and_nights = [
 			{
@@ -79,6 +171,8 @@ define(['core', 'logger', 'd3', 'jquery', 'plugins/editor', 'utils/data', 'utils
 		];
 
 		plugin.data.scenes = scenes;
+
+
 	};
 
 	plugin.deactivate = function () {
