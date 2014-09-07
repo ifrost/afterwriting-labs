487ab0566f803b34500d30945b55fa0ad912bb7d
Piotr Jamroz <pm.jamroz@gmail.com>
2014-09-07 13:14:09 +0100

    bumpup for versioning

--- js/utils/common.js
+++ js/utils/common.js
@@ -3,7 +3,7 @@ define(function () {
 	return {
 		data: {
 			static_path: 'bundle/',
-			footer: 'version: <a href="http://www.imdb.com/title/tt1790885/" target="_blank"><span class="version">Zero</span>&nbsp;Dark Thrirty</a>'
+			footer: ''
 		}
 	};
 
