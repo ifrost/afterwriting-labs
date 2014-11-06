9a96d8af3c348d23f81d1f75e5ec3c12255d3b62
Piotr Jamroz <pm.jamroz@gmail.com>
2014-11-06 22:40:28 +0000

    stats: refresh after expand

--- js/plugins/stats.js
+++ js/plugins/stats.js
@@ -15,6 +15,7 @@ define(function (require) {
 	};
 
 	plugin.activate = function () {
+		plugin.is_active = true;
 		plugin.data.days_and_nights = queries.days_and_nights.run(data.parsed.tokens);
 		plugin.data.scenes = queries.scene_length.run(data.parsed.tokens);
 		var basics = queries.basics.run(data.parsed.lines);
@@ -25,7 +26,7 @@ define(function (require) {
 	};
 
 	plugin.deactivate = function () {
-		log.info('stats:deactivate');
+		plugin.is_active = false;
 	};
 
 	return plugin;
