52ecb121b311fee34c22be3ccc7cc182163b181d
Piotr Jamroz <pm.jamroz@gmail.com>
2014-12-28 18:27:02 +0000

    pdf: fix for non-English letters in character's name

--- js/utils/fountain/parser.js
+++ js/utils/fountain/parser.js
@@ -183,7 +183,7 @@ define(function (require) {
 						token.number = match[1];
 					}
 					scene_number++;
-				} else if (token.text.match(regex.character)) {
+				} else if (token.text === token.text.toUpperCase()) {					
 					if (i === lines_length || i === lines_length - 1 || lines[i + 1].length === 0) {
 						token.type = 'shot';
 					} else {
