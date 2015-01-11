1d28cfb7511e1ea7693baf95eee83d3eee069f30
Piotr Jamroz <pm.jamroz@gmail.com>
2015-01-11 19:30:33 +0000

    save: save to google drive

--- js/plugins/save.js
+++ js/plugins/save.js
@@ -4,6 +4,7 @@ define(function (require) {
 	var pm = require('utils/pluginmanager'),
 		saveAs = require('saveAs'),
 		preview = require('plugins/preview'),
+		gd = require('utils/googledrive'),
 		data = require('modules/data');
 
 	var plugin = pm.create_plugin('save', 'save');
@@ -32,13 +33,30 @@ define(function (require) {
 	};
 
 	plugin.dropbox_pdf = function () {
-		var uri = preview.get_pdf(function(data){
+		var uri = preview.get_pdf(function (data) {
 			Dropbox.save(data.url, plugin.data.filename + '.pdf');
-		});		
+		});
+	};
+
+	plugin.google_drive_fountain = function () {
+		var blob = new Blob([data.script()], {
+			type: "text/plain;charset=utf-8"
+		});
+		gd.save(blob, plugin.data.filename + '.fountain');
+	};
+
+	plugin.google_drive_pdf = function () {
+		var uri = preview.get_pdf(function (data) {
+			gd.save(data.blob, plugin.data.filename + '.pdf');
+		});
 	};
 
 	plugin.is_dropbox_available = function () {
 		return window.Dropbox !== undefined && Dropbox.isBrowserSupported() && window.location.protocol !== 'file:';
+	};
+
+	plugin.is_google_drive_available = function () {
+		return window.gapi && window.location.protocol !== 'file:';
 	};
 
 	plugin.activate = function () {
--- js/utils/googledrive.js
+++ js/utils/googledrive.js
@@ -1,4 +1,4 @@
-/* global define, gapi, google */
+/* global define, gapi, google, $, FileReader, btoa */
 define(function () {
 	var client_id = '540351787353-3jf0j12ccl0tmv2nbkcdncu0tuegjkos.apps.googleusercontent.com',
 		dev_key = 'AIzaSyAakDfF6BAzFrLZ-ZWCht2lpYx7i9ITZrY',
@@ -11,19 +11,25 @@ define(function () {
 	module.open = function (content_callback) {
 		module.current_content_callback = content_callback;
 		gapi.auth.init(function () {
-			authorize(true, authorized.bind(this));
+			authorize(true, open_picker.bind(this));
 		});
 	};
 
-	function authorize(immediate, callback) {
-		gapi.auth.authorize({
-			client_id: client_id,
-			scope: scope,
-			immediate: immediate,
-		}, callback);
+	module.save = function (blob, filename) {
+		gapi.auth.init(function () {
+			authorize(true, save_file.bind(this, blob, filename));
+		});
+	};
+
+	function save_file(blob, filename, result) {
+		if (result && !result.error) {
+			upload(blob, filename);
+		} else {
+			authorize(false, save_file.bind(this, blob, filename));
+		}
 	}
 
-	function authorized(result) {
+	function open_picker(result) {
 		if (result && !result.error) {
 			module.oauth_token = result.access_token;
 			new google.picker.PickerBuilder()
@@ -34,8 +40,16 @@ define(function () {
 				.build()
 				.setVisible(true);
 		} else {
-			authorize(false, authorized);
+			authorize(false, open_picker);
 		}
+	}
+
+	function authorize(immediate, callback) {
+		gapi.auth.authorize({
+			client_id: client_id,
+			scope: scope,
+			immediate: immediate,
+		}, callback);
 	}
 
 	function picker_ready(data) {
@@ -51,12 +65,12 @@ define(function () {
 		}).execute(function (response) {
 
 			if (!response.error) {
-				download(response.exportLinks['text/plain'], function(content) {
+				download(response.exportLinks['text/plain'], function (content) {
 					module.current_content_callback(content, response.alternateLink);
 				});
 			}
 		});
-	};
+	}
 
 	function download(url, callback) {
 		$.ajax({
@@ -67,7 +81,48 @@ define(function () {
 			},
 			success: callback
 		});
-	};
+	}
+
+	function upload(blob, filename) {
+		var boundary = '-------314159265358979323846';
+		var delimiter = "\r\n--" + boundary + "\r\n";
+		var close_delim = "\r\n--" + boundary + "--";
+		var reader = new FileReader();
+		reader.readAsBinaryString(blob);
+		reader.onload = function () {
+			var contentType = blob.type || 'application/octet-stream';
+			var metadata = {
+				'title': filename,
+				'mimeType': contentType
+			};
+
+			var base64Data = btoa(reader.result);
+			var multipartRequestBody =
+				delimiter +
+				'Content-Type: application/json\r\n\r\n' +
+				JSON.stringify(metadata) +
+				delimiter +
+				'Content-Type: ' + contentType + '\r\n' +
+				'Content-Transfer-Encoding: base64\r\n' +
+				'\r\n' +
+				base64Data +
+				close_delim;
+
+			var request = gapi.client.request({
+				'path': '/upload/drive/v2/files',
+				'method': 'POST',
+				'params': {
+					'uploadType': 'multipart'
+				},
+				'headers': {
+					'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+				},
+				'body': multipartRequestBody
+			});
+
+			request.execute();
+		};
+	}
 
 	return module;
 
