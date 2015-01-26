5f24e8a085cbeee315491a8066e246248176e1bf
Piotr Jamroz <pm.jamroz@gmail.com>
2015-01-26 01:08:10 +0000

    open: refactor

--- js/plugins/open.js
+++ js/plugins/open.js
@@ -80,11 +80,11 @@ define(function (require) {
 		return window.gapi && window.location.protocol !== 'file:';
 	};
 
-	plugin.open_from_dropbox = function () {
-		db.list(function (root) {
-			root = db.convert_to_jstree(root);
+	var open_from_cloud = function (client, back_callback, load_callback) {
+		client.list(function (root) {
+			root = client.convert_to_jstree(root);
 			tree.show({
-				label: 'Open from Dropbox',
+				info: 'Please select file to open.',
 				data: [root],
 				callback: function (selected) {
 					if (selected.data.isFolder) {
@@ -95,49 +95,33 @@ define(function (require) {
 							},
 							submit: function (v, e, f, m) {
 								if (v) {
-									plugin.open_from_dropbox();
+									back_callback()
 								}
 							}
 						});
 					} else {
-						db.load_file(selected.data.path, function (content) {
-							set_script(content);
-							data.data('db-path', selected.data.path);
-						});
+						load_callback(selected);
 					}
 				}
 			});
 		});
-		
+	}
+
+	plugin.open_from_dropbox = function () {
+		open_from_cloud(db, plugin.open_from_dropbox, function (selected) {
+			db.load_file(selected.data.path, function (content) {
+				set_script(content);
+				data.data('db-path', selected.data.path);
+			});
+		});		
 	};
 
 	plugin.open_from_google_drive = function () {
-		gd.list(function (root) {
-			root = gd.convert_to_jstree(root);
-			tree.show({
-				label: 'Open',
-				data: [root],
-				callback: function (selected) {
-					if (selected.data.isFolder) {
-						$.prompt('Please select a file, not folder.', {
-							buttons: {
-								'Back': true,
-								'Cancel': false
-							},
-							submit: function (v, e, f, m) {
-								if (v) {
-									plugin.open_from_google_drive();
-								}
-							}
-						});
-					} else {
-						gd.load_file(selected.data.id, function (content, link, fileid) {
-							set_script(content);
-							data.data('gd-link', link);
-							data.data('gd-fileid', fileid);
-						});
-					}
-				}
+		open_from_cloud(gd, plugin.open_from_google_drive, function (selected) {
+			gd.load_file(selected.data.id, function (content, link, fileid) {
+				set_script(content);
+				data.data('gd-link', link);
+				data.data('gd-fileid', fileid);
 			});
 		});
 	};
@@ -174,7 +158,7 @@ define(function (require) {
 				editor.activate(); // refresj
 			}
 		});
-		save.db_saved.add(function(path) {
+		save.db_saved.add(function (path) {
 			clear_last_opened();
 			data.data('db-path', path);
 			data.data('filename', '');
