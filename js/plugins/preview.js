9e6a722c0fe643a74cedd2ce732f6aba8125dace
Piotr Jamroz <pm.jamroz@gmail.com>
2015-07-10 00:47:15 +0100

    pdf test

--- js/plugins/preview.js
+++ js/plugins/preview.js
@@ -8,7 +8,38 @@ define(function (require) {
 	var plugin = pm.create_plugin('preview', 'view');
 	
 	plugin.get_pdf = function(callback) {
-		pdfmaker.get_pdf(callback);
+		PDFJS.disableWebGL = false;
+		pdfmaker.get_pdf(function(data){
+			setTimeout(function(){
+				PDFJS.getDocument(data.url).then(function(pdf) {
+					// Using promise to fetch the page
+					pdf.getPage(2).then(function(page) {
+						var scale = 1.1;
+						var viewport = page.getViewport(scale);
+
+						//
+						// Prepare canvas using PDF page dimensions
+						//
+						var canvas = document.getElementById('pdf-canvas');
+						var context = canvas.getContext('2d');
+						canvas.height = viewport.height;
+						canvas.width = viewport.width;
+
+						//
+						// Render PDF page into canvas context
+						//
+						var renderContext = {
+							canvasContext: context,
+							viewport: viewport
+						};
+						page.render(renderContext);
+						callback(data);
+					});
+				}).catch(function(error){
+					console.log(error)
+				});
+			}, 100);
+		});
 	};
 	
 	plugin.refresh = decorator.signal();		
