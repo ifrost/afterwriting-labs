e22e34d44ceccf0d87e93c6cfe9e33285ba0e8ea
Piotr Jamroz <pm.jamroz@gmail.com>
2015-11-20 00:41:12 +0000

    change default font encoding from MacRoman to WinAnsi

--- js/libs/pdfkit.js
+++ js/libs/pdfkit.js
@@ -557,7 +557,7 @@ PDFFont = (function() {
       FirstChar: firstChar,
       LastChar: firstChar + charWidths.length - 1,
       Widths: charWidths,
-      Encoding: 'MacRomanEncoding',
+      Encoding: 'WinAnsiEncoding',
       ToUnicode: cmap
     };
     return this.dictionary.end();
