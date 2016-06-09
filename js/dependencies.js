763442da51bae0001c64dfba0c6ebb1eaac042d0
Piotr Jamroz <pm.jamroz@gmail.com>
2016-06-09 21:32:06 +0100

    keep ACCEPTANCE flag in one place

--- js/dependencies.js
+++ js/dependencies.js
@@ -1,8 +1,3 @@
 define(['jquery', 'impromptu', 'jstree', 'cookie'], function($) {
-   if (window.location.search.indexOf('ACCEPTANCE') !== -1 || navigator.userAgent.indexOf('PhantomJS') !== -1) {
-      // XXX: if you turn the workers on, the acceptance test will fail because
-      // call executed by workers is asynchronous and the test does not take this
-      // into account
-      $.jstree.defaults.core.worker = false;
-   }
+   $.jstree.defaults.core.worker = JS_TREE_WORKERS_ENABLED;
 });
