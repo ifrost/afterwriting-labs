96f08d5e188cf7672ce9733bc9136b014e84f0c2
Piotr Jamroz <pm.jamroz@gmail.com>
2016-06-15 00:04:46 +0100

    clean up bootstrap

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,27 +1,28 @@
 define(['dependencies', 'p'], function(_, p) {
 
-    var bootstrap = {};
-
-    bootstrap.init = function(modules) {
-
-        this.modules = Array.prototype.splice.call(modules, 0);
-
-        var context = p.Context.create();
-
-        context.register('core', this);
-
-        this.modules.forEach(function(module) {
-            context.register(module.name, module);
-        }, this);
-
-        context.build();
+    var Bootstrap = p.extend({
         
-        this.modules.forEach(function(module) {
-            if (module.windup && typeof (module.windup) === 'function') {
-                module.windup();
-            }
-        });
-    };
+        init: function(modules) {
 
-    return bootstrap;
+            this.modules = Array.prototype.splice.call(modules, 0);
+
+            var context = p.Context.create();
+
+            context.register('core', this);
+
+            this.modules.forEach(function(module) {
+                context.register(module.name, module);
+            }, this);
+
+            context.build();
+
+            this.modules.forEach(function(module) {
+                if (module.windup && typeof (module.windup) === 'function') {
+                    module.windup();
+                }
+            });
+        }
+    });
+
+    return Bootstrap.create();
 });
