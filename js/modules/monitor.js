45c82d645649c35b75200ff7c2b997e7a5639ec4
Piotr Jamroz <pm.jamroz@gmail.com>
2016-06-15 22:08:27 +0100

    monitor as a Module

--- js/modules/monitor.js
+++ js/modules/monitor.js
@@ -10,106 +10,109 @@ define(function(require) {
         stats = require('plugins/stats'),
         layout = require('plugins/layout');
 
-    var module = Module.create('monitor');
-    var log = logger.get('monitor');
+    var Monitor = Module.extend({
 
-    var track_event = function(category, action, label) {
-        if (window.ga) {
-            log.info('Event sent', category, action, label || '');
-            ga('send', 'event', category, action, label);
-        } else {
-            log.debug('Event not sent:', category, action, label || '', ' [Google Analytics not loaded.]');
+        name: 'monitor',
+
+        prepare: function() {
+            if (window.location.protocol !== 'file:') {
+                (function(i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function() {
+                            (i[r].q = i[r].q || []).push(arguments);
+                        };
+                    i[r].l = 1 * new Date();
+                    a = s.createElement(o);
+                    m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m);
+                })(window, document, 'script', 'http://www.google-analytics.com/analytics.js', 'ga');
+
+                ga('create', 'UA-53953908-1', 'auto');
+                ga('send', 'pageview');
+            }
+        },
+
+        windup: function() {
+            // layout stats
+            layout.scopes.toolbar_switch_to.add(function(plugin) {
+                this.track_event('navigation', plugin.name, 'toolbar');
+            }.bind(this));
+            layout.scopes.main_switch_to.add(function(plugin) {
+                this.track_event('navigation', plugin.name, 'main');
+            }.bind(this));
+            layout.scopes.switcher_switch_to.add(function(plugin) {
+                this.track_event('navigation', plugin.name, 'switcher');
+            }.bind(this));
+            layout.scopes.toolbar_close_content.add(function(plugin) {
+                this.track_event('navigation', 'toolbar-close', plugin.name);
+            }.bind(this));
+            layout.scopes.back_close_content.add(function(plugin) {
+                this.track_event('navigation', 'back-close', plugin.name);
+            }.bind(this));
+            layout.info_opened.add(function(section) {
+                this.track_event('feature', 'help', section);
+            }.bind(this));
+            layout.toggle_expand.add(this.track_handler('feature', 'expand'));
+
+            // info
+            info.download_clicked.add(this.track_handler('feature', 'download'));
+
+            // open
+            open.open_sample.add(function(result) {
+                this.track_event('feature', 'open-sample', result);
+            }.bind(this));
+
+            open.create_new.add(this.track_handler('feature', 'open-new'));
+            open.open_file_dialog.add(this.track_handler('feature', 'open-file-dialog'));
+            open.open_file.add(function(format) {
+                this.track_event('feature', 'open-file-opened', format);
+            }.bind(this));
+            open.open_from_dropbox.add(function(format) {
+                this.track_event('feature', 'open-dropbox', format);
+            }.bind(this));
+            open.open_from_google_drive.add(function(format) {
+                this.track_event('feature', 'open-googledrive', format);
+            }.bind(this));
+            open.open_last_used.add(function(startup) {
+                this.track_event('feature', 'open-last-used', startup === true ? 'startup' : 'manual');
+            }.bind(this));
+
+            // editor
+            editor.synced.add(function(cloud) {
+                this.track_event('feature', 'sync', cloud);
+            }.bind(this));
+
+            // save
+            save.save_as_fountain.add(this.track_handler('feature', 'save-fountain'));
+            save.save_as_pdf.add(this.track_handler('feature', 'save-pdf'));
+            save.dropbox_fountain.add(this.track_handler('feature', 'save-fountain-dropbox'));
+            save.google_drive_fountain.add(this.track_handler('feature', 'save-fountain-googledrive'));
+            save.dropbox_pdf.add(this.track_handler('feature', 'save-pdf-dropbox'));
+            save.google_drive_pdf.add(this.track_handler('feature', 'save-pdf-googledrive'));
+
+            // stats
+            stats.goto.add(this.track_handler('feature', 'stats-scene-length-goto'));
+
+        },
+
+        track_event: function(category, action, label) {
+            if (window.ga) {
+                this.logger.info('Event sent', category, action, label || '');
+                ga('send', 'event', category, action, label);
+            } else {
+                this.logger.debug('Event not sent:', category, action, label || '', ' [Google Analytics not loaded.]');
+            }
+        },
+
+        track_handler: function(category, action, label) {
+            return function() {
+                this.track_event(category, action, label);
+            }.bind(this);
         }
-    };
 
-    var track_handler = function(category, action, label) {
-        return function() {
-            track_event(category, action, label);
-        };
-    };
+    });
 
-    module.prepare = function() {
-        if (window.location.protocol !== 'file:') {
-            (function(i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function() {
-                        (i[r].q = i[r].q || []).push(arguments);
-                    };
-                i[r].l = 1 * new Date();
-                a = s.createElement(o);
-                m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m);
-            })(window, document, 'script', 'http://www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-53953908-1', 'auto');
-            ga('send', 'pageview');
-        }
-    };
-
-    module.windup = function() {
-        // layout stats
-        layout.scopes.toolbar_switch_to.add(function(plugin) {
-            track_event('navigation', plugin.name, 'toolbar');
-        });
-        layout.scopes.main_switch_to.add(function(plugin) {
-            track_event('navigation', plugin.name, 'main');
-        });
-        layout.scopes.switcher_switch_to.add(function(plugin) {
-            track_event('navigation', plugin.name, 'switcher');
-        });
-        layout.scopes.toolbar_close_content.add(function(plugin) {
-            track_event('navigation', 'toolbar-close', plugin.name);
-        });
-        layout.scopes.back_close_content.add(function(plugin) {
-            track_event('navigation', 'back-close', plugin.name);
-        });
-        layout.info_opened.add(function(section) {
-            track_event('feature', 'help', section);
-        });
-        layout.toggle_expand.add(track_handler('feature', 'expand'));
-
-        // info
-        info.download_clicked.add(track_handler('feature', 'download'));
-
-        // open
-        open.open_sample.add(function(result) {
-            track_event('feature', 'open-sample', result);
-        });
-
-        open.create_new.add(track_handler('feature', 'open-new'));
-        open.open_file_dialog.add(track_handler('feature', 'open-file-dialog'));
-        open.open_file.add(function(format) {
-            track_event('feature', 'open-file-opened', format);
-        });
-        open.open_from_dropbox.add(function(format) {
-            track_event('feature', 'open-dropbox', format);
-        });
-        open.open_from_google_drive.add(function(format) {
-            track_event('feature', 'open-googledrive', format);
-        });
-        open.open_last_used.add(function(startup) {
-            track_event('feature', 'open-last-used', startup === true ? 'startup' : 'manual');
-        });
-
-        // editor
-        editor.synced.add(function(cloud) {
-            track_event('feature', 'sync', cloud);
-        });
-
-        // save 
-        save.save_as_fountain.add(track_handler('feature', 'save-fountain'));
-        save.save_as_pdf.add(track_handler('feature', 'save-pdf'));
-        save.dropbox_fountain.add(track_handler('feature', 'save-fountain-dropbox'));
-        save.google_drive_fountain.add(track_handler('feature', 'save-fountain-googledrive'));
-        save.dropbox_pdf.add(track_handler('feature', 'save-pdf-dropbox'));
-        save.google_drive_pdf.add(track_handler('feature', 'save-pdf-googledrive'));
-
-        // stats
-        stats.goto.add(track_handler('feature', 'stats-scene-length-goto'));
-
-    };
-
-    return module;
+    return Monitor.create();
 });
