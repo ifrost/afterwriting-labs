b860ccb57ef737a77e5901a59c1c7cd4f35e3dfd
Piotr Jamroz <pm.jamroz@gmail.com>
2016-06-15 22:50:51 +0100

    settings plugin

--- js/plugins/settings.js
+++ js/plugins/settings.js
@@ -1,32 +1,46 @@
 define(function(require) {
-    
+
     var Plugin = require('core/plugin'),
         template = require('text!templates/plugins/settings.hbs'),
-        data = require('modules/data'),
-        layout = require('plugins/layout'),
         open = require('plugins/open');
 
-    var plugin = Plugin.create('settings', 'setup', template);
+    var Settings = Plugin.extend({
 
-    plugin.get_config = function() {
-        return data.config;
-    };
+        name: 'settings',
 
-    plugin.save = function() {
-        data.save_config();
-        data.script(data.script());
-    };
+        title: 'setup',
 
-    plugin.get_default_config = function() {
-        return data.default_config;
-    };
+        template: template,
 
-    plugin.windup = function() {
-        if (data.config.load_last_opened) {
-            open.open_last_used(true);
-            layout.show_main();
+        data: {
+            inject: 'data'
+        },
+
+        layout: {
+            inject: 'layout'
+        },
+
+        get_config: function() {
+            return this.data.config;
+        },
+
+        save: function() {
+            this.data.save_config();
+            this.data.script(this.data.script());
+        },
+
+        get_default_config: function() {
+            return this.data.default_config;
+        },
+
+        windup: function() {
+            if (this.data.config.load_last_opened) {
+                open.open_last_used(true);
+                this.layout.show_main();
+            }
         }
-    };
 
-    return plugin;
+    });
+
+    return Settings.create();
 });
