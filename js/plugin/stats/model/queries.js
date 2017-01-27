6003c1012b85a585b1beab7de843f3dfe6db50bf
Piotr Jamroz <pm.jamroz@gmail.com>
2017-01-27 23:03:29 +0000

    Remove data dependency from queries

--- js/plugin/stats/model/queries.js
+++ js/plugin/stats/model/queries.js
@@ -1,6 +1,5 @@
 define(function(require) {
     var helper = require('utils/helper'),
-        data = require('modules/data'),
         fquery = require('utils/fountain/query'),
         fhelpers = require('utils/fountain/helpers');
 
@@ -10,33 +9,39 @@ define(function(require) {
 
     var create_days_and_nights = function() {
 
-        var query = fquery('label', {
-            value: 0
-        });
-        query.prepare(function(fq) {
-            fq.recognized_scenes = 0;
-        });
-        query.count('value', h.has_scene_time('DAY'), 'DAY', true)
-            .count('value', h.has_scene_time('NIGHT'), 'NIGHT', true)
-            .count('value', h.has_scene_time('DUSK'), 'DUSK', true)
-            .count('value', h.has_scene_time('DAWN'), 'DAWN', true);
-        query.enter(h.is('scene_heading'), function(item, fq) {
-            if (data.config.stats_keep_last_scene_time && fq.last_selection) {
-                fq.last_selection.value++;
-            }
-        });
-        query.exit(function(item, fq) {
-            fq.recognized_scenes += item.value;
-        });
-        query.end(function(result, fq) {
-            var all_scenes = fquery().count('scenes', h.is('scene_heading')).run(fq.source).scenes;
-
-            result.push({
-                label: 'OTHER',
-                value: all_scenes - fq.recognized_scenes
+        var runner = {};
+        
+        runner.run = function(tokens, stats_keep_last_scene_time) {
+            var query = fquery('label', {
+                value: 0
             });
-        });
-        return query;
+            query.prepare(function(fq) {
+                fq.recognized_scenes = 0;
+            });
+            query.count('value', h.has_scene_time('DAY'), 'DAY', true)
+                .count('value', h.has_scene_time('NIGHT'), 'NIGHT', true)
+                .count('value', h.has_scene_time('DUSK'), 'DUSK', true)
+                .count('value', h.has_scene_time('DAWN'), 'DAWN', true);
+            query.enter(h.is('scene_heading'), function(item, fq) {
+                if (stats_keep_last_scene_time && fq.last_selection) {
+                    fq.last_selection.value++;
+                }
+            });
+            query.exit(function(item, fq) {
+                fq.recognized_scenes += item.value;
+            });
+            query.end(function(result, fq) {
+                var all_scenes = fquery().count('scenes', h.is('scene_heading')).run(fq.source).scenes;
+
+                result.push({
+                    label: 'OTHER',
+                    value: all_scenes - fq.recognized_scenes
+                });
+            });
+            return query.run(tokens);
+        };
+        
+        return runner;
     };
 
     var create_int_and_ext = function() {
@@ -52,31 +57,35 @@ define(function(require) {
     };
 
     var create_scene_length = function() {
-        var query = fquery('token', {
-            length: 0
-        });
-        query.prepare(function(fq) {
-            fq.current_header = undefined;
-        });
-        query.enter(h.is('scene_heading'), function(token, fq) {
-            fq.current_header = fq.select(token);
-            fq.current_header.header = token.text;
-            fq.current_header.location_type = token.location_type();
-        });
-        query.enter(true, function(item, fq) {
-            if (fq.current_header) {
-                fq.current_header.length += item.lines.length;
-                if (fq.current_header.token.has_scene_time('DAY')) {
-                    fq.current_header.type = 'day';
-                } else if (fq.current_header.token.has_scene_time('NIGHT')) {
-                    fq.current_header.type = 'night';
-                } else {
-                    fq.current_header.type = fq.last_header_type || 'other';
+        var runner = {};
+        runner.run = function(tokens, stats_keep_last_scene_time) {
+            var query = fquery('token', {
+                length: 0
+            });
+            query.prepare(function(fq) {
+                fq.current_header = undefined;
+            });
+            query.enter(h.is('scene_heading'), function(token, fq) {
+                fq.current_header = fq.select(token);
+                fq.current_header.header = token.text;
+                fq.current_header.location_type = token.location_type();
+            });
+            query.enter(true, function(item, fq) {
+                if (fq.current_header) {
+                    fq.current_header.length += item.lines.length;
+                    if (fq.current_header.token.has_scene_time('DAY')) {
+                        fq.current_header.type = 'day';
+                    } else if (fq.current_header.token.has_scene_time('NIGHT')) {
+                        fq.current_header.type = 'night';
+                    } else {
+                        fq.current_header.type = fq.last_header_type || 'other';
+                    }
+                    fq.last_header_type = stats_keep_last_scene_time ? fq.current_header.type : undefined;
                 }
-                fq.last_header_type = data.config.stats_keep_last_scene_time ? fq.current_header.type : undefined;
-            }
-        });
-        return query;
+            });
+            return query.run(tokens);
+        };
+        return runner;
     };
 
     var apply_character_levels = function(plan, basics) {
--- js/plugin/stats/view/stats-view-presenter.js
+++ js/plugin/stats/view/stats-view-presenter.js
@@ -25,9 +25,9 @@ define(function(require) {
 
             var statsData = {};
             // editor.synced.add(plugin.refresh);
-            statsData.days_and_nights = queries.days_and_nights.run(this.scriptModel.parsed_stats.tokens);
+            statsData.days_and_nights = queries.days_and_nights.run(this.scriptModel.parsed_stats.tokens, this.scriptModel.config.stats_keep_last_scene_time);
             statsData.int_and_ext = queries.int_and_ext.run(this.scriptModel.parsed_stats.tokens);
-            statsData.scenes = queries.scene_length.run(this.scriptModel.parsed_stats.tokens);
+            statsData.scenes = queries.scene_length.run(this.scriptModel.parsed_stats.tokens, this.scriptModel.config.stats_keep_last_scene_time);
             var basics = this.scriptModel.getBasicStats();
             statsData.who_with_who = queries.dialogue_breakdown.run(this.scriptModel.parsed_stats.tokens, basics, this.scriptModel.config.stats_who_with_who_max);
             statsData.page_balance = queries.page_balance.run(this.scriptModel.parsed_stats.lines);
