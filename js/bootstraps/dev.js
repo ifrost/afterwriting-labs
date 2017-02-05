78734aaea8e9cf9eef6fa523093d8c48361a1248
Piotr Jamroz <pm.jamroz@gmail.com>
2017-02-05 00:16:17 +0000

    Run tests or dev config depending on site setup

--- js/bootstraps/dev.js
+++ js/bootstraps/dev.js
@@ -1,4 +1,10 @@
 require(['dependencies', 'bootstrap', 'bootstraps/dev-config', '../test/acceptance/setup'], function (_, Bootstrap, DevConfig, AcceptanceTestsSetup) {
-    var setup = AcceptanceTestsSetup.create(Bootstrap, DevConfig);
-    setup.run();
+    // DEBT: split acceptance and dev bootstrap
+    if (window.ACCEPTANCE) {
+        var setup = AcceptanceTestsSetup.create(Bootstrap, DevConfig);
+        setup.run();
+    }
+    else {
+        Bootstrap.init(DevConfig);
+    }
 });
