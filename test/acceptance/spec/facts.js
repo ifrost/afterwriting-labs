2827dd988b127b9e1cfb39f48e761a96d2437c64
Piotr Jamroz <pm.jamroz@gmail.com>
2017-02-19 01:05:15 +0000

    Move a facts plugin test to facts.js

--- test/acceptance/spec/facts.js
+++ test/acceptance/spec/facts.js
@@ -13,9 +13,28 @@ define(function (require) {
         afterEach(function() {
             env.destroy();
         });
-        
-        it('GIVEN synchronisation is enabled WHEN facts plugin is selected AND content changes THEN facts are refreshed', function() {
-            
+
+        it('GIVEN synchronisation is enabled WHEN facts plugin is selected AND content changes THEN facts are refreshed', function(done) {
+
+            // GIVEN
+            env.scenarios.load_dropbox_file({
+                name: 'file.fountain',
+                content: 'INT. TEST'
+            }, function() {
+                env.user.theme.open_plugin('editor');
+                env.user.editor.turn_sync_on();
+
+                // WHEN
+                env.user.theme.open_plugin('facts');
+                env.assert.facts.number_of_scenes_is(1);
+
+                // AND
+                env.scenarios.dropbox_file_changes('file.fountain', 'INT. TEST\n\nINT. TEST', function() {
+                    // THEN
+                    env.assert.facts.number_of_scenes_is(2);
+                    done();
+                });
+            });
         });
         
     });
--- test/acceptance/spec/stats.js
+++ test/acceptance/spec/stats.js
@@ -29,30 +29,6 @@ define(function (require) {
             env.assert.facts.number_of_scenes_is(2);
         });
 
-        // TODO: move to facts.js (+)
-        it('GIVEN synchronisation is enabled WHEN facts plugin is selected AND content changes THEN facts are refreshed', function(done) {
-
-            // GIVEN
-            env.scenarios.load_dropbox_file({
-                name: 'file.fountain',
-                content: 'INT. TEST'
-            }, function() {
-                env.user.theme.open_plugin('editor');
-                env.user.editor.turn_sync_on();
-
-                // WHEN
-                env.user.theme.open_plugin('facts');
-                env.assert.facts.number_of_scenes_is(1);
-
-                // AND
-                env.scenarios.dropbox_file_changes('file.fountain', 'INT. TEST\n\nINT. TEST', function() {
-                    // THEN
-                    env.assert.facts.number_of_scenes_is(2);
-                    done();
-                });
-            });
-        });
-
         it('GIVEN synchronisation is enabled WHEN stats plugin is selected AND content changes THEN stats are refreshed', function(done) {
             // GIVEN
             env.scenarios.load_dropbox_file({
